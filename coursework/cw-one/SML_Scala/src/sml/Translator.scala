a33a216eb3199e3527f8bce782a4bd471a2b8535
Iszak Bryan <iszak.bryan@gmail.com>
2017-02-03 16:06:14 +0000

    Added support for multiplication

--- coursework/cw-one/SML_Scala/src/sml/Translator.scala
+++ coursework/cw-one/SML_Scala/src/sml/Translator.scala
@@ -28,6 +28,8 @@ class Translator(fileName: String) {
             program = program :+ AddInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
           case LIN =>
             program = program :+ LinInstruction(fields(0), fields(2).toInt, fields(3).toInt)
+          case MUL =>
+            program = program :+ MulInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
           case x =>
             println(s"Unknown instruction $x")
         }

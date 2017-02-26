0c3d2f6a5a13cc42a0cc2d90cc4095020ac25b77
Iszak Bryan <iszak.bryan@gmail.com>
2017-02-26 16:44:41 +0000

    Implemented IConstByteCode

---
+++ coursework/cw-two/src/main/scala/bc/IConstByteCode.scala
@@ -0,0 +1,21 @@
+package bc
+import vm.VirtualMachine
+
+class IConstByteCode(num:Int) extends ByteCode {
+  /**
+    * A unique byte value representing the bytecode. An implementation
+    * will set this to the bytecode corresponding to the name of the
+    * bytecode in [[ByteCodeValues]]
+    */
+  override val code: Byte = 1
+
+  /**
+    * Returns a new [[VirtualMachine]] after executing this bytecode operation.
+    *
+    * @param vm the initial virtual machine
+    * @return a new virtual machine
+    */
+  override def execute(vm: VirtualMachine): VirtualMachine = {
+    vm.push(num)
+  }
+}

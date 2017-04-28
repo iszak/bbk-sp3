11b42d2cf82aa18ea7deb20e6754e54b8ae8b463
Iszak Bryan <iszak.bryan@gmail.com>
2017-04-28 19:48:56 +0100

    Implemented virtual proxy

--- exercises/week11/src/main/scala/proxy/virtualproxy/ContactListProxyImpl.scala
+++ exercises/week11/src/main/scala/proxy/virtualproxy/ContactListProxyImpl.scala
@@ -4,8 +4,15 @@ import scala.collection.mutable.ListBuffer
 
 class ContactListProxyImpl extends ContactList {
 
-  private var contactList: ContactList = ???
+  private var contactList: ContactList = _
 
-  override def employeeList: ListBuffer[Employee] = ???
+  override def employeeList: ListBuffer[Employee] = contactList match {
+    case cl: ContactList => cl.employeeList
+    case _ => {
+      println("Creating contact list and fetching list of employees...")
+      contactList = ContactListImpl()
+      contactList.employeeList
+    }
+  }
 
 }
--- exercises/week11/src/main/scala/proxy/virtualproxy/Employee.scala
+++ exercises/week11/src/main/scala/proxy/virtualproxy/Employee.scala
@@ -6,5 +6,7 @@ case class Employee(
   var employeeDesignation: String
 ) {
 
-  override def toString(): String = ???
+  override def toString(): String = s"Employee Name: $employeeName, " +
+    s"EmployeeDesignation: $employeeDesignation, " +
+    s"Employee Salary: $employeeSalary"
 }

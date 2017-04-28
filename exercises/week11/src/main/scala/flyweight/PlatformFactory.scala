9f061c812422013fe1c5bbe67c5eccf9efb2d309
Iszak Bryan <iszak.bryan@gmail.com>
2017-04-28 12:34:21 +0100

    Implemented flyweight pattern

--- exercises/week11/src/main/scala/flyweight/PlatformFactory.scala
+++ exercises/week11/src/main/scala/flyweight/PlatformFactory.scala
@@ -5,9 +5,20 @@ import java.util.Map
 
 object PlatformFactory {
 
-  private var map: Map[String, Platform] = ???
+  private var map: Map[String, Platform] = new HashMap()
 
-  def getPlatformInstance(platformType: String): Platform = ???
+  def getPlatformInstance(platformType: String): Platform = map.containsKey(platformType) match {
+    case true => map.get(platformType)
+    case false => {
+      map.put(platformType, platformType.toUpperCase match {
+        case "SCALA" => new ScalaPlatform()
+        case "C" => new CPlatform()
+        case "JAVA" => new JavaPlatform()
+        case "RUBY" => new RubyPlatform()
+      })
+      map.get(platformType)
+    }
+  }
   // this method will probably need to be synchronized
 
 }
